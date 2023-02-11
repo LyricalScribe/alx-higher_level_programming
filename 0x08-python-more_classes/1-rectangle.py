0ce360ebfbd53e8c6cf9c7045dfe53bb1ffe75f2
Thulisil Ngomane <ThuleeN@outlook.com>
2023-02-11 20:02:20 +0200

    Added 2-rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""empty class Rectangle"""
+
+
+class Rectangle:
+    """ empty class Rectangle """
+
+    def __init__ (self,width=0, height=0):
+        """ initializing data """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ sets the width of the instance """
+        return self.__width
+        
+    @width.setter
+    def width(self, value):
+        """ gets the width of a rectangle class"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be an integer")
+        else:
+            self.__width = value
+            
+            
+    @property   
+    def height(self):
+        """ sets the height of the instance """
+        return self.__height
+        
+    @height.setter
+    def height(self, value):
+        """ gets the height of a rectangle class """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")  
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+        
+    
