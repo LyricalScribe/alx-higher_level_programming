a2a3f164677ff68c70c22413a53f27a17a6e6e34
Thulisil Ngomane <ThuleeN@outlook.com>
2023-03-15 23:48:44 +0200

    Added tasks

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const oldSq = require('./5-square');
+class Square extends oldSq {
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
+
