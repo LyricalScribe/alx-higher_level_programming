9f6933dd0a383544a6fc58b47fbf0545d9a3559e
Thulisile Ngomane <thuleen@outlook.com>
2023-04-01 00:46:48 +0200

    status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Script to fect data from a url """
+
+if __name__ == "__main__":
+	from urllib import request
+	
+	url = 'https://alx-intranet.hbtn.io/status'
+	with request.urlopen(url) as response:
+		content = response.read()
+		print("Body response:")
+		print("\t - type: {}".format(type(content)))
+		print("\t - content: {}".format(content))
+		print("\t - utf8 content: {}".format(content.decode('utf-8')))
