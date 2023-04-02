5473e654dab63286a5549cb6def829700bde25ea
Thulisile Ngomane <thuleen@outlook.com>
2023-04-02 23:16:52 +0200

    fixed indentation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,12 @@
 """ Script to fect data from a url """
 
 if __name__ == "__main__":
- from urllib import request
+    import urllib.request
 
     url = 'https://alx-intranet.hbtn.io/status'
 
     with request.urlopen(url) as response:
-    content = response.read()
+        content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
