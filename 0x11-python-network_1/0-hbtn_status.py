dae505f1ef99d9e17cc21c73ba468404434cc628
Thulisile Ngomane <thuleen@outlook.com>
2023-04-02 22:46:59 +0200

    Script edits

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,11 @@
 if __name__ == "__main__":
  from urllib import request
 
- url = 'https://alx-intranet.hbtn.io/status'
- with request.urlopen(url) as response:
-   content = response.read()
-   print("Body response:")
-   print("\t - type: {}".format(type(content)))
-   print("\t - content: {}".format(content))
-   print("\t - utf8 content: {}".format(content.decode('utf-8')))
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with request.urlopen(url) as response:
+    content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,10 +2,10 @@
 """script tp fetch url from request"""
 
 if __name__ == "__main__":
-	from urllib import request
-	from sys import argv
+    from urllib import request
+    from sys import argv
 
-	url = argv[1]
-	with request.urlopen(url) as response:
-		request_id = response.getheader('X-Request-Id')
-		print(request_id)
+    url = argv[1]
+    with request.urlopen(url) as response:
+        request_id = response.getheader('X-Request-Id')
+        print(request_id)
