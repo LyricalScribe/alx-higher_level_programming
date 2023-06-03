17699172d959474fb8986878126b1f8d86051ca4
Thulisile <ThuleeN@outlook.com>
2023-06-03 17:06:39 +0200

    Added files

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Defines an object atrribute"""
+
+def lookup(obj):
+	"""Return a list of class atrribute"""
+	return (dir(obj))
+	
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Defines a function that checks for a class"""
+
+def is_same_class(obj, a_class):
+    """Checks if an object is exactly an instance of a given class"""
+
+    return isinstance(obj, a_class)
+   
