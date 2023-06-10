b08b4cec7805767996a41dfa747a7512e84ea8a7
Thulisile <ThuleeN@outlook.com>
2023-06-10 17:27:59 +0200

    fixed pycodestyle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,18 +2,17 @@
 
 """Defines Base model class"""
 
+
 class Base():
-	"""The Base model class"""
+    """The Base model class"""
 
-	__nb_objects = 0
+    __nb_objects = 0
 
-	def __init__(self, id=None):
-		"""Args: id(int), new initialized Base"""
+    def __init__(self, id=None):
+        """Args: id(int), new initialized Base"""
 
-		if id is not None:
-			self.id = id
-		else:
-			Base.__nb_objects += 1
-			self.id = Base__nb_objects
-
-		
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base__nb_objects
