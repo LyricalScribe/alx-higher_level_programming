cfb24b80f9e5148578171fd2d0421aa03fa1409b
Thulisile <111003983+LyricalScribe@users.noreply.github.com>
2023-06-11 21:49:54 +0200

    additional comments added to square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,7 +7,18 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize Square object"""
+        """Initialize Square object
+
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        Raises:
+            TypeError: If the size is not an int.
+            ValueError: If e size is <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y <= 0."""
         super().__init__(size, size, x, y, id)
 
     @property
@@ -31,7 +42,16 @@ class Square(Rectangle):
         self.__height = value
 
     def update(self, *args, **kwargs):
-        """update method for the square object"""
+        """update method for the square object
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
         if args and len(args) != 0:
             a = 0
             for arg in args:
