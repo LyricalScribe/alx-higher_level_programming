32f60f93a2e29ff4df411d0c54e7eb6813db5e31
Thulisile <111003983+LyricalScribe@users.noreply.github.com>
2023-06-11 21:58:08 +0200

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,56 +1,42 @@
 #!/usr/bin/python3
-
-"""defines Square class"""
+"""Defines square class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """New Square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize Square object
+        """Initialization of Square;
 
         Args:
-            size (int): The size of the new Square.
-            x (int): The x coordinate of the new Square.
-            y (int): The y coordinate of the new Square.
-            id (int): The identity of the new Square.
-        Raises:
-            TypeError: If the size is not an int.
-            ValueError: If e size is <= 0.
-            TypeError: If either of x or y is not an int.
-            ValueError: If either of x or y <= 0."""
+            size (int): The size of the new Square
+            x (int): The x coordinate of the new Square
+            y (int): The y coordinate of the new Square
+            id (int): The identity of the new Square
+        """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """getter for the size of the Square object"""
+        """Gets/sets size of Square"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """setter for the size of the Square object"""
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
         self.width = value
-
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
+        self.height = value
 
     def update(self, *args, **kwargs):
-        """update method for the square object
+        """Square updates;
 
         Args:
-            *args (ints): New attribute values.
+            *args (ints): New attribute values
                 - 1st argument represents id attribute
                 - 2nd argument represents size attribute
                 - 3rd argument represents x attribute
                 - 4th argument represents y attribute
-            **kwargs (dict): New key/value pairs of attributes.
+            **kwargs (dict): Key/value pairs of attributes
         """
         if args and len(args) != 0:
             a = 0
@@ -83,7 +69,7 @@ class Square(Rectangle):
                     self.y = v
 
     def to_dictionary(self):
-        """Returns the dict of this object"""
+        """Returns the dict of Square"""
         return {
             "id": self.id,
             "size": self.width,
@@ -92,6 +78,6 @@ class Square(Rectangle):
         }
 
     def __str__(self):
-        """Returns str() of the Square object"""
+        """Returns the print() and str() of Square"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
